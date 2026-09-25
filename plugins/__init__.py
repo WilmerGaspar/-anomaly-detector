4343bdd01473becd3f96c145b2c724531eb2d057
WilmerGaspar <wilmergasparespinoza@gmail.com>
2026-09-25 07:38:01 -0600

    Add periodicity plugin and materials-family mapper

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,17 +1,21 @@
-"""
-Plugins matemáticos para versión web.
-"""
+"""Plugins de descriptores de imagen. Todos calculan métricas reales (sin placeholders)."""
 
 from .fractal_base import FractalBase
 from .kolmogorov_1941 import Kolmogorov1941
 from .lyapunov_stability import LyapunovStability
 from .persistent_homology import PersistentHomology
 from .renormalization_group import RenormalizationGroup
+from .anisotropy import calculate_anisotropy
+from .entropy import calculate_entropy
+from .periodicity import analyze_periodicity
 
 __all__ = [
-    'FractalBase',
-    'Kolmogorov1941',
-    'LyapunovStability',
-    'PersistentHomology',
-    'RenormalizationGroup'
+    "FractalBase",
+    "Kolmogorov1941",
+    "LyapunovStability",
+    "PersistentHomology",
+    "RenormalizationGroup",
+    "calculate_anisotropy",
+    "calculate_entropy",
+    "analyze_periodicity",
 ]
